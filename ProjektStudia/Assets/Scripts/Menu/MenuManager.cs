5680c8f081a02601a272681d3fafd9915e84338f
JakubKondej <162355075+JakubKondej@users.noreply.github.com>
2025-05-06 19:51:25 +0200

    Summary
    Wyciąłem trochę kodu i przeniosłem w jedno miejsce oraz scaliłem settings panel z resolution panel.
    
    Przy okazji naprawiłem błąd z wczytywaniem preferencji głośności w menu głównym.

--- ProjektStudia/Assets/Scripts/Menu/MenuManager.cs
+++ ProjektStudia/Assets/Scripts/Menu/MenuManager.cs
@@ -1,13 +1,16 @@
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private GameObject mainMenuPanel;
-    [SerializeField] private GameObject optionsMenuPanel;
-    [SerializeField] private GameObject soundSettingsPanel;
-    [SerializeField] private GameObject resolutionSettingsPanel;
+    [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private AudioClip buttonSoundClip;
+
+    private void Start()
+    {
+        LoadSettings();
+    }
 
     public async void PlayGame()
     {
@@ -20,48 +23,14 @@ public class MenuManager : MonoBehaviour
     {
         SoundFXManager.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
         await Task.Delay(100);
-
-        mainMenuPanel.SetActive(false);
-        optionsMenuPanel.SetActive(true);
-        soundSettingsPanel.SetActive(false);
-        resolutionSettingsPanel.SetActive(false);
-    }
-
-    public async void OpenSoundSettings()
-    {
-        SoundFXManager.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
-        await Task.Delay(100);
-
-        optionsMenuPanel.SetActive(false);
-        soundSettingsPanel.SetActive(true);
-    }
-
-    public async void OpenResolutionSettings()
-    {
-        SoundFXManager.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
-        await Task.Delay(100);
-
-        optionsMenuPanel.SetActive(false);
-        resolutionSettingsPanel.SetActive(true);
-    }
-
-    public async void BackToOptionsMenu()
-    {
-        SoundFXManager.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
-        await Task.Delay(100);
-
-        optionsMenuPanel.SetActive(true);
-        soundSettingsPanel.SetActive(false);
-        resolutionSettingsPanel.SetActive(false);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Settings");
     }
 
     public async void BackToMainMenu()
     {
         SoundFXManager.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
         await Task.Delay(100);
-
-        optionsMenuPanel.SetActive(false);
-        mainMenuPanel.SetActive(true);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
     }
 
     public async void ExitGame()
@@ -70,5 +39,26 @@ public class MenuManager : MonoBehaviour
         await Task.Delay(1000);
         Application.Quit();
         Debug.Log("Quit game");
+    }
+
+    private void LoadSettings()
+    {
+        if (PlayerPrefs.HasKey("masterVolume"))
+        {
+            float savedVolume = PlayerPrefs.GetFloat("masterVolume");
+            audioMixer.SetFloat("masterVolume", Mathf.Log10(savedVolume) * 20f);
+        }
+
+        if (PlayerPrefs.HasKey("soundFXVolume"))
+        {
+            float savedFXVolume = PlayerPrefs.GetFloat("soundFXVolume");
+            audioMixer.SetFloat("soundFXVolume", Mathf.Log10(savedFXVolume) * 20f);
+        }
+
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            float savedMusicVolume = PlayerPrefs.GetFloat("musicVolume");
+            audioMixer.SetFloat("musicVolume", Mathf.Log10(savedMusicVolume) * 20f);
+        }
     }
 }
