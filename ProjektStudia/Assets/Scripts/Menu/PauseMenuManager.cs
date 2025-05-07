5d97bb892ff1f06c81bc07e98814b82cace00402
Wiktoria Szyller <157953747+SzyllerW@users.noreply.github.com>
2025-05-07 21:08:59 +0200

    Revert "Summary"

--- ProjektStudia/Assets/Scripts/Menu/PauseMenuManager.cs
+++ ProjektStudia/Assets/Scripts/Menu/PauseMenuManager.cs
@@ -5,17 +5,23 @@ using UnityEngine.SceneManagement;
 public class PauseMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenuPanel;
-    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject[] settingsPanels;
+    [SerializeField] private GameObject navigationPanel;
+    [SerializeField] private GameObject leftArrowButton;
+    [SerializeField] private GameObject rightArrowButton;
     [SerializeField] private GameObject descriptionPanel;
     [SerializeField] private AudioClip buttonSoundClip;
     [SerializeField] private GameObject pauseButton;
 
     private bool isPaused = false;
+    private int currentSettingsIndex = 0;
 
     void Start()
     {
         pauseMenuPanel.SetActive(false);
+        HideAllSettingsPanels();
         descriptionPanel.SetActive(false);
+        navigationPanel.SetActive(false);
         pauseButton.SetActive(true);
     }
 
@@ -55,7 +61,9 @@ public class PauseMenuManager : MonoBehaviour
         await Task.Delay(100);
 
         pauseMenuPanel.SetActive(true);
+        HideAllSettingsPanels();
         descriptionPanel.SetActive(false);
+        navigationPanel.SetActive(false);
 
         Time.timeScale = 0f;
         isPaused = true;
@@ -68,7 +76,9 @@ public class PauseMenuManager : MonoBehaviour
         await Task.Delay(100);
 
         pauseMenuPanel.SetActive(false);
+        HideAllSettingsPanels();
         descriptionPanel.SetActive(false);
+        navigationPanel.SetActive(false);
 
         Time.timeScale = 1f;
         isPaused = false;
@@ -81,8 +91,77 @@ public class PauseMenuManager : MonoBehaviour
         await Task.Delay(100);
 
         pauseMenuPanel.SetActive(false);
-        settingsPanel.SetActive(true);
+        navigationPanel.SetActive(true);
+        ResetNavigation(); 
+        ShowSettingsPanel(currentSettingsIndex);
+
         descriptionPanel.SetActive(false);
+    }
+
+    public void ShowNextSettingsPanel()
+    {
+        currentSettingsIndex = (currentSettingsIndex + 1) % settingsPanels.Length;
+        ShowSettingsPanel(currentSettingsIndex);
+    }
+
+    public void ShowPreviousSettingsPanel()
+    {
+        currentSettingsIndex--;
+        if (currentSettingsIndex < 0)
+            currentSettingsIndex = settingsPanels.Length - 1;
+
+        ShowSettingsPanel(currentSettingsIndex);
+    }
+
+    private void ShowSettingsPanel(int index)
+    {
+        for (int i = 0; i < settingsPanels.Length; i++)
+        {
+            settingsPanels[i].SetActive(i == index);
+        }
+
+        UpdateNavigationArrows();
+    }
+
+    private void HideAllSettingsPanels()
+    {
+        foreach (GameObject panel in settingsPanels)
+        {
+            panel.SetActive(false);
+        }
+    }
+
+    private void UpdateNavigationArrows()
+    {
+        if (settingsPanels.Length <= 1)
+        {
+            leftArrowButton.SetActive(false);
+            rightArrowButton.SetActive(false);
+            return;
+        }
+
+        if (currentSettingsIndex == 0)
+        {
+            leftArrowButton.SetActive(false);
+            rightArrowButton.SetActive(true);
+        }
+        else if (currentSettingsIndex == settingsPanels.Length - 1)
+        {
+            leftArrowButton.SetActive(true);
+            rightArrowButton.SetActive(false);
+        }
+        else
+        {
+            leftArrowButton.SetActive(true);
+            rightArrowButton.SetActive(true);
+        }
+    }
+
+    private void ResetNavigation()
+    {
+        currentSettingsIndex = 0; 
+        leftArrowButton.SetActive(false); 
+        rightArrowButton.SetActive(true);
     }
 
     public async void BackToPauseMenu()
@@ -91,8 +170,9 @@ public class PauseMenuManager : MonoBehaviour
         await Task.Delay(100);
 
         pauseMenuPanel.SetActive(true);
-        settingsPanel.SetActive(false);
+        HideAllSettingsPanels();
         descriptionPanel.SetActive(false);
+        navigationPanel.SetActive(false);
     }
 
     public async void GoToMainMenu()
@@ -112,7 +192,9 @@ public class PauseMenuManager : MonoBehaviour
         if (!descriptionPanel.activeSelf)
         {
             pauseMenuPanel.SetActive(false);
+            HideAllSettingsPanels();
             descriptionPanel.SetActive(true);
+            navigationPanel.SetActive(false);
 
             Time.timeScale = 0f;
             isPaused = true;
@@ -121,6 +203,7 @@ public class PauseMenuManager : MonoBehaviour
         {
             descriptionPanel.SetActive(false);
             pauseMenuPanel.SetActive(true);
+            navigationPanel.SetActive(false);
         }
     }
 }
